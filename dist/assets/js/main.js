95a184a013aa668698e6f9c59abed21f2b95ead8
Luciana Castro <lucianaycastro@gmail.com>
2017-10-03 00:44:06 -0300

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -24,31 +24,33 @@ $(function() {
 	   	$('html, body').animate({
 	       scrollTop: $("#capabilities-section").offset().top - 150
 	   	}, 600);
+	   	$(".nav-menu").fadeToggle('fast');
 	});
 
 	$("#pricing-button").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#pricing-section").offset().top
 		}, 600);
+		$(".nav-menu").fadeToggle('fast');
 	});
 
 	$("#more-button").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#more-section").offset().top
 		}, 600);
+		$(".nav-menu").fadeToggle('fast');
 	});
 
 	$("#contact-button").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#contact-section").offset().top
 		}, 600);
+		$(".nav-menu").fadeToggle('fast');
 	});
 
 	/*Mobile*/
 	$(".toggle-button").click(function() { 
-		$(".nav-menu").fadeToggle('fast', function(){
-
-		});
+		$(".nav-menu").fadeToggle('fast');
 	});
 	
 });
